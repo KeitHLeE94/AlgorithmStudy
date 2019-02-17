ed05094488cdd393d502afce89c1711191e4611a
KeitHLeE94 <wogus4187@naver.com>
2019-02-17 14:45:29 +0900

    2/17 문제풀이

--- Baekjoon/16235.cpp
+++ Baekjoon/16235.cpp
@@ -13,10 +13,6 @@ struct Tree{
     int x, y, age;
 };
 
-//struct deadTree{ // 죽은 나무 x좌표, y좌표, 나이
-//    int x, y, age;
-//};
-
 int N, M, K;
 
 int map[11][11] = {0, };
@@ -45,20 +41,6 @@ int BFS(){
 
         sort(lives.begin(), lives.end(), compare);
 
-        for(int j=1; j<=N; j++){
-            for(int k=1; k<=N; k++){
-                cout << map[j][k] << ' ';
-            }
-            cout << '\n';
-        }
-        cout << '\n';
-
-        for(int j=0; j<lives.size(); j++){
-            cout << lives[j].age << ' ';
-        }
-        cout << '\n';
-        cout << '\n';
-
         for(int j=0; j<lives.size(); j++) {
             if (map[lives[j].x][lives[j].y] >= lives[j].age) { // 봄
                 map[lives[j].x][lives[j].y] -= lives[j].age;
@@ -67,9 +49,10 @@ int BFS(){
             }
             else {
                 deads.push_back({lives[j].x, lives[j].y, lives[j].age});
+                lives[j].age = -1;
             }
 
-            if(lives[j].age % 5 == 0){ // 가을
+            if(lives[j].age > 0 && lives[j].age % 5 == 0){ // 가을
                 for(int k=0; k<8; k++){
                     int nextX = lives[j].x + dx[k];
                     int nextY = lives[j].y + dy[k];
@@ -90,63 +73,10 @@ int BFS(){
                 map[k][l] += winterAdd[k][l];
             }
         }
-
-        for(int j=1; j<=N; j++){
-            for(int k=1; k<=N; k++){
-                cout << map[j][k] << ' ';
-            }
-            cout << '\n';
-        }
-
-        cout << "======================================" << '\n';
     }
 
     return Queue.size();
 }
-
-//void years(){
-//    for(int i=1; i<=N; i++){
-//        for(int j=1; j<=N; j++){
-//            if(tree[i][j].size() > 0){
-//                vector<int> deads; // 죽은 나무들
-//
-//                sort(tree[i][j].begin(), tree[i][j].end());
-//
-//                cout << tree[i][j][0] << '\n';
-//
-//                for(int k=0; k<tree[i][j].size(); k++){ // 봄
-//                    if(tree[i][j][k] > 0 && map[i][j] >= tree[i][j][k]){ // 나무가 양분을 먹었다
-//                        map[i][j] -= tree[i][j][k];
-//                        tree[i][j][k]++;
-//                    }
-//                    else{ // 나무가 양분을 못먹고 죽었다
-//                        deads.push_back(tree[i][j][k]);
-//                        tree[i][j][k] = -1;
-//                    }
-//                }
-//
-//                for(int k=0; k<deads.size(); k++){ // 여름
-//                    map[i][j] += (deads[k] / 2);
-//                }
-//
-//                for(int k=0; k<tree[i][j].size(); k++){ // 가을
-//                    if(tree[i][j][k] % 5 == 0){ // 나무의 나이가 5의 배수이면 번식
-//                        tree[i-1][j-1].push_back(1);
-//                        tree[i-1][j].push_back(1);
-//                        tree[i-1][j+1].push_back(1);
-//                        tree[i][j-1].push_back(1);
-//                        tree[i][j+1].push_back(1);
-//                        tree[i+1][j-1].push_back(1);
-//                        tree[i+1][j].push_back(1);
-//                        tree[i+1][j+1].push_back(1);
-//                    }
-//                }
-//
-//                map[i][j] += winterAdd[i][j]; // 겨울
-//            }
-//        }
-//    }
-//}
 
 int main(){
     for(int i=0; i<=10; i++){
@@ -172,11 +102,6 @@ int main(){
     }
 
     cout << BFS() << '\n';
-
-    while(!Queue.empty()){
-        cout << Queue.front().x << ", " << Queue.front().y << ": " << Queue.front().age << '\n';
-        Queue.pop();
-    }
 
     return 0;
 }
