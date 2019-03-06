eadf74dd493338e14e047c380b73686076a7ec0d
KeitHLeE94 <wogus4187@naver.com>
2019-03-06 23:54:10 +0900

    2/6 문제풀이

--- Baekjoon/16988.cpp
+++ Baekjoon/16988.cpp
@@ -4,7 +4,8 @@
 
 #include <iostream>
 #include <queue>
-#include <vector>
+#include <strings.h>
+#include <memory.h>
 
 using namespace std;
 
@@ -12,47 +13,71 @@ int N, M;
 
 int map[20][20];
 int tempMap[20][20];
+bool visit[20][20];
+
 int dx[] = {0, 0, 1, -1};
 int dy[] = {1, -1, 0, 0};
 
-void BFS(){
-    int countMap[20][20];
+int maxValue = 0;
+bool finish = false;
 
-    for(int i=0; i<N; i++){
-        for(int j=0; j<M; j++){
-            countMap[i][j] = tempMap[i][j];
-        }
-    }
+int BFS(int x, int y){
+    int count = 0;
 
-    for(int i=0; i<N; i++){
-        for(int j=0; j<M; j++){
-            if(countMap[i][j] == 2){
-                for(int k=0; k<4; k++){
-                    int nextX = i + dx[k];
-                    int nextY = j + dy[k];
+    queue<pair<int, int>> Queue;
 
-                    if(nextX >= 0 && nextX < N && nextY >= 0 && nextY < M){
-                        if(countMap[nextX][nextY] == 0){
-                            countMap[i][j] = 3;
-                        }
-                    }
+    memcpy(map, tempMap, sizeof(tempMap));
+
+    Queue.push(make_pair(x, y));
+
+    count++;
+
+    while(!Queue.empty()){
+        int currentX = Queue.front().first;
+        int currentY = Queue.front().second;
+        Queue.pop();
+
+        for(int i=0; i<4; i++){
+            int nextX = currentX + dx[i];
+            int nextY = currentY + dy[i];
+
+            if(nextX >= 0 && nextX < N && nextY >= 0 && nextY < M){
+                if(!visit[nextX][nextY] && map[nextX][nextY] == 2){
+                    visit[nextX][nextY] = true;
+                    count++;
+                    Queue.push(make_pair(nextX, nextY));
+                }
+                else if(map[nextX][nextY] == 0){
+                    finish = true;
                 }
             }
         }
     }
 
-    for(int i=0; i<N; i++){
-        for(int j=0; j<M; j++){
-            cout << countMap[i][j] << ' ';
-        }
-        cout << '\n';
+    if(finish){
+        return 0;
     }
-    cout << '\n';
+
+    return count;
 }
 
 void DFS(int count){
     if(count == 2){
-        BFS();
+        int temp = 0;
+        bzero(visit, sizeof(visit));
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                if(!visit[i][j] && tempMap[i][j] == 2){
+                    finish = false;
+                    visit[i][j] = true;
+                    temp += BFS(i, j);
+
+                    maxValue = maxValue < temp ? temp : maxValue;
+                }
+            }
+        }
+
         return;
     }
 
@@ -79,6 +104,8 @@ int main(){
     }
 
     DFS(0);
+
+    cout << maxValue << '\n';
 
     return 0;
 }
