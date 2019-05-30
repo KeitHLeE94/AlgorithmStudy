6bbcc146c5739e462baee7bfb7192191ea62d60f
KeitHLeE94 <wogus4187@naver.com>
2019-05-30 21:03:05 +0900

    5/30 문제풀이(답 틀림)

--- Baekjoon/17143.cpp
+++ Baekjoon/17143.cpp
@@ -16,13 +16,11 @@ struct shark{
 
 int R, C, M;
 
-bool cmp(shark a, shark b){
-    if(a.size < b.size){
-        return false;
+struct cmp{
+    bool operator()(shark a, shark b){
+        return a.size < b.size;
     }
-
-    return true;
-}
+};
 
 bool map[101][101] = {false, };
 
@@ -32,76 +30,68 @@ pair<pair<int, int>, int> move(shark toMove){
     int y = toMove.y;
     int dir = toMove.dir;
 
-    if(toMove.dir == 1){
+    if(dir == 1){ // 상
         int dx = -1;
 
         while(speed > 0){
-            if(x == 1 || x == R){
-                dx *= -1;
-
-                if(dir == 1){
-                    dir = 2;
-                }
-                else if(dir == 2){
-                    dir = 1;
-                }
+            if(x == 1){
+                dx = 1;
+                dir = 2;
+            }
+            else if(x == R){
+                dx = -1;
+                dir = 1;
             }
 
             x += dx;
             speed--;
         }
     }
-    else if(toMove.dir == 2){
+    else if(dir == 2){ // 하
         int dx = 1;
 
         while(speed > 0){
-            if(x == 1 || x == R){
-                dx *= -1;
-
-                if(dir == 1){
-                    dir = 2;
-                }
-                else if(dir == 2){
-                    dir = 1;
-                }
+            if(x == 1){
+                dx = 1;
+                dir = 2;
+            }
+            else if(x == R){
+                dx = -1;
+                dir = 1;
             }
 
             x += dx;
             speed--;
         }
     }
-    else if(toMove.dir == 3){
+    else if(dir == 3){ // 우
         int dy = 1;
 
         while(speed > 0){
-            if(y == 1 || y == C){
-                dy *= -1;
-
-                if(dir == 3){
-                    dir = 4;
-                }
-                else if(dir == 4){
-                    dir = 3;
-                }
+            if(y == 1){
+                dy = 1;
+                dir = 3;
+            }
+            else if(y == C){
+                dy = -1;
+                dir = 4;
             }
 
             y += dy;
             speed--;
         }
     }
-    else if(toMove.dir == 4){
+    else if(dir == 4){ // 좌
         int dy = -1;
 
         while(speed > 0){
-            if(y == 1 || y == C){
-                dy *= -1;
-
-                if(dir == 3){
-                    dir = 4;
-                }
-                else if(dir == 4){
-                    dir = 3;
-                }
+            if(y == 1){
+                dy = 1;
+                dir = 3;
+            }
+            else if(y == C){
+                dy = -1;
+                dir = 4;
             }
 
             y += dy;
@@ -115,62 +105,70 @@ pair<pair<int, int>, int> move(shark toMove){
 int main(){
     cin >> R >> C >> M;
 
-    vector<shark> alive;
+    priority_queue<shark, vector<shark>, cmp> alive;
 
     for(int i=0; i<M; i++){
         int r, c, s, d, z;
 
         cin >> r >> c >> s >> d >> z;
 
-        alive.push_back({r, c, s, d, z});
+        alive.push({r, c, s, d, z});
     }
 
     int result = 0;
 
-    for(int fisher=1; fisher<=C; fisher++){
-        int minX = 9999;
-        int minIndex = 0;
+    if(M != 0) {
+        for (int fisher = 1; fisher <= C; fisher++) {
+            int minX = 9999;
+            int minIndex = 9999;
 
-        for(int i=0; i<M; i++){
-            if(alive[i].y == fisher){
-                if(alive[i].x < minX){
-                    minX = alive[i].x;
-                    minIndex = i;
+            vector<shark> temp;
+            while(!alive.empty()){
+                temp.push_back(alive.top());
+                alive.pop();
+            }
+
+            for (int i = 0; i < temp.size(); i++) {
+//                cout << temp[i].x << ", " << temp[i].y << ": " << temp[i].size << '\n';
+                if (temp[i].y == fisher) {
+                    if (temp[i].x < minX) {
+                        minX = temp[i].x;
+                        minIndex = i;
+                    }
                 }
             }
-        }
 
-        result += alive[minIndex].size;
-        alive[minIndex].size = -1;
+            if(temp[minIndex].x > 0 && temp[minIndex].y > 0){
+                result += temp[minIndex].size;
+                temp[minIndex].size = -1;
 
-        sort(alive.begin(), alive.end(), cmp);
-
-        vector<shark> temp;
-        for(int i=0; i<M; i++){
-            if(alive[i].size >= 0){
-                temp.push_back(alive[i]);
+//                cout << "잡힘: " << temp[minIndex].x << ", " << temp[minIndex].y << ": " << temp[minIndex].size << '\n';
             }
-        }
 
-        alive.clear();
-        bzero(map, sizeof(map));
+            bzero(map, sizeof(map));
 
-        for(int i=0; i<temp.size(); i++){
-            cout << temp[i].x << ", " << temp[i].y << ": " << temp[i].speed << " | ";
-            pair<pair<int, int>, int> moved = move(temp[i]);
+            for (int i = 0; i < temp.size(); i++) {
+                if(temp[i].size >= 0){
+//                    cout << temp[i].x << ", " << temp[i].y << ": " << temp[i].size << '\n';
 
-            int nextX = moved.first.first;
-            int nextY = moved.first.second;
-            int nextDir = moved.second;
+                    pair<pair<int, int>, int> moved = move(temp[i]);
 
-            cout << nextX << ", " << nextY << " - " << nextDir << '\n';
+                    int nextX = moved.first.first;
+                    int nextY = moved.first.second;
+                    int nextDir = moved.second;
 
-            if(!map[nextX][nextY]){
-                temp[i].x = nextX;
-                temp[i].y = nextY;
-                temp[i].dir = nextDir;
-                map[nextX][nextY] = true;
+                    if (!map[nextX][nextY]) {
+                        temp[i].x = nextX;
+                        temp[i].y = nextY;
+                        temp[i].dir = nextDir;
+                        map[nextX][nextY] = true;
+
+                        alive.push(temp[i]);
+                    }
+                }
             }
+
+//            cout << "=============================================" << '\n';
         }
     }
 
