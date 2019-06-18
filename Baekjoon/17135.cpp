f908afca831b1b334df97c3e179cccbde5525849
KeitHLeE94 <wogus4187@naver.com>
2019-06-18 16:50:34 +0900

    6/18 문제풀이

--- Baekjoon/17135.cpp
+++ Baekjoon/17135.cpp
@@ -4,26 +4,25 @@
 
 #include <iostream>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
 struct Enemy{
-    int x, y;
-    bool alive;
+    int x, y; // 적 위치
+    bool alive; // 적 생존여부
 };
 
 int N, M, D;
 int map[16][16] = {0, };
 
-int distance(int archerY, int enemyX, int enemyY){
-    return enemyX + abs(archerY - enemyY);
+int distance(int archerY, int enemyX, int enemyY){ // 거리 구하는 함수
+    return abs((N+1) - enemyX) + abs(archerY - enemyY);
 }
 
 int main(){
     cin >> N >> M >> D;
 
-    vector<Enemy> realEnemies;
+    vector<Enemy> realEnemies; // 입력으로 주어진 적들
 
     for(int i=1; i<=N; i++){
         for(int j=1; j<=M; j++){
@@ -35,52 +34,34 @@ int main(){
         }
     }
 
-    int maxKill = -1;
+    int maxKill = -1; // 최대값
 
+    // 궁수 위치 조합
     for(int i=1; i<=M; i++){
         for(int j=i+1; j<=M; j++){
             for(int k=j+1; k<=M; k++){
-                cout << "archers: ";
-                cout << i << ' ' << j << ' ' << k << '\n';
-                int kill = 0;
+                int kill = 0; // 현재 위치에서 킬수
 
-                vector<Enemy> enemies;
+                vector<Enemy> enemies; // 현재 살아있는 적들
 
-                for(int e=0; e<realEnemies.size(); e++){
+                for(int e=0; e<realEnemies.size(); e++){ // 입력으로 주어진 적들로 현재 살아있는 적들 vector 초기화
                     enemies.push_back(realEnemies[e]);
                 }
 
-                while(!enemies.empty()){
-//                    int tempMap[16][15] = {0, };
-//
-//                    for(int e=0; e<enemies.size(); e++){
-//                        tempMap[enemies[e].x][enemies[e].y] = 1;
-//                    }
-//
-//                    cout << "Before" << '\n';
-//
-//                    for(int r=1; r<=N; r++){
-//                        for(int c=0; c<M; c++){
-//                            cout << tempMap[r][c] << ' ';
-//                        }
-//                        cout << '\n';
-//                    }
-
-                    int firstDist = D;
-                    int firstY = M;
-                    int firstKill = -1;
+                while(!enemies.empty()){ // 적이 전부 없어질때까지 반복
+                    int firstDist = D; // 첫번째 궁수가 죽일 적까지의 거리
+                    int firstY = M+1; // 첫번째 궁수가 죽일 적의 위치(왼쪽에서 몇번째인지)
+                    int firstKill = -1; // 첫번째 궁수가 죽일 적의 vector 내 인덱스
                     for(int first=0; first<enemies.size(); first++){
-                        cout << enemies[first].x << ", " << enemies[first].y << ": ";
-                        int temp = distance(i, enemies[first].x, enemies[first].y);
-                        cout << temp << '\n';
+                        int temp = distance(i, enemies[first].x, enemies[first].y); // 첫번째 궁수와 적과의 거리
 
-                        if(temp < firstDist){
+                        if(temp < firstDist){ // 더 가까운 곳에 적이 있는 경우
                             firstY = enemies[first].y;
                             firstDist = temp;
                             firstKill = first;
                         }
                         else if(temp == firstDist){
-                            if(enemies[first].y < firstY){
+                            if(enemies[first].y < firstY){ // 적과의 거리가 같고 현재 타겟보다 왼쪽에 적이 있는 경우
                                 firstY = enemies[first].y;
                                 firstDist = temp;
                                 firstKill = first;
@@ -88,28 +69,24 @@ int main(){
                         }
                     }
 
-                    if(firstKill >= 0 && enemies[firstKill].alive){
-                        cout << "first: (" << enemies[firstKill].x << ", " << enemies[firstKill].y << ")\n";
-                        kill++;
-                        enemies[firstKill].alive = false;
+                    if(firstKill >= 0 && enemies[firstKill].alive){ // 이미 죽은 적이 아닐 경우
+                        kill++; // 킬수 늘림
+                        enemies[firstKill].alive = false; // 맞은 적 죽었다고 표시
                     }
-                    cout << '\n';
 
-                    int secondDist = D;
-                    int secondY = M;
-                    int secondKill = -1;
+                    int secondDist = D; // 두번째 궁수가 죽일 적까지의 거리
+                    int secondY = M+1; // 두번째 궁수가 죽일 적의 위치(왼쪽에서 몇번째인지)
+                    int secondKill = -1; // 두번째 궁수가 죽일 적의 vector 내 인덱스
                     for(int second=0; second<enemies.size(); second++){
-                        cout << enemies[second].x << ", " << enemies[second].y << ": ";
-                        int temp = distance(j, enemies[second].x, enemies[second].y);
-                        cout << temp << '\n';
+                        int temp = distance(j, enemies[second].x, enemies[second].y); // 두번째 궁수와 적과의 거리
 
-                        if(temp < secondDist){
+                        if(temp < secondDist){ // 더 가까운 곳에 적이 있는 경우
                             secondY = enemies[second].y;
                             secondDist = temp;
                             secondKill = second;
                         }
                         else if(temp == secondDist){
-                            if(enemies[second].y < secondY){
+                            if(enemies[second].y < secondY){ // 적과의 거리가 같고 현재 타겟보다 왼쪽에 적이 있는 경우
                                 secondY = enemies[second].y;
                                 secondDist = temp;
                                 secondKill = second;
@@ -117,28 +94,24 @@ int main(){
                         }
                     }
 
-                    if(secondKill >= 0 && enemies[secondKill].alive){
-                        cout << "second: (" << enemies[secondKill].x << ", " << enemies[secondKill].y << ")\n";
-                        kill++;
-                        enemies[secondKill].alive = false;
+                    if(secondKill >= 0 && enemies[secondKill].alive){ // 이미 죽은 적이 아닐 경우
+                        kill++; // 킬수 늘림
+                        enemies[secondKill].alive = false; // 맞은 적 죽었다고 표시
                     }
-                    cout << '\n';
 
-                    int thirdDist = D;
-                    int thirdY = M;
-                    int thirdKill = -1;
+                    int thirdDist = D; // 세번째 궁수가 죽일 적까지의 거리
+                    int thirdY = M+1; // 세번째 궁수가 죽일 적의 위치(왼쪽에서 몇번째인지)
+                    int thirdKill = -1; // 세번째 궁수가 죽일 적의 vector 내 인덱스
                     for(int third=0; third<enemies.size(); third++){
-                        cout << enemies[third].x << ", " << enemies[third].y << ": ";
-                        int temp = distance(k, enemies[third].x, enemies[third].y);
-                        cout << temp << '\n';
+                        int temp = distance(k, enemies[third].x, enemies[third].y); // 세번째 궁수와 적과의 거리
 
-                        if(temp < thirdDist){
+                        if(temp < thirdDist){ // 더 가까운 곳에 적이 있는 경우
                             thirdY = enemies[third].y;
                             thirdDist = temp;
                             thirdKill = third;
                         }
                         else if(temp == thirdDist){
-                            if(enemies[third].y < thirdY){
+                            if(enemies[third].y < thirdY){ // 적과의 거리가 같고 현재 타겟보다 왼쪽에 적이 있는 경우
                                 thirdY = enemies[third].y;
                                 thirdDist = temp;
                                 thirdKill = third;
@@ -146,52 +119,32 @@ int main(){
                         }
                     }
 
-                    if(thirdKill >= 0 && enemies[thirdKill].alive){
-                        cout << "third: (" << enemies[thirdKill].x << ", " << enemies[thirdKill].y << ")\n";
-                        kill++;
-                        enemies[thirdKill].alive = false;
+                    if(thirdKill >= 0 && enemies[thirdKill].alive){ // 이미 죽은 적이 아닐 경우
+                        kill++; // 킬수 늘림
+                        enemies[thirdKill].alive = false; // 맞은 적 죽었다고 표시
                     }
 
-                    vector<Enemy> aliveEnemy;
+                    vector<Enemy> aliveEnemy; // 라운드 끝난 후 살아있는 적
 
                     for(int ae=0; ae<enemies.size(); ae++){
-                        if(enemies[ae].alive){
-                            enemies[ae].x--;
+                        if(enemies[ae].alive){ // 한 라운드 진행 후 살아있는 적들에 대해
+                            enemies[ae].x++; // 성쪽으로 한칸 이동
 
-                            if(enemies[ae].x > 0){
-                                aliveEnemy.push_back(enemies[ae]);
+                            if(enemies[ae].x <= N){ // 이동 후 성에 닿지 않았으면
+                                aliveEnemy.push_back(enemies[ae]); // 라운드 끝난 후 살아있는 적 vector에 추가
                             }
                         }
                     }
 
-                    enemies.clear();
+                    enemies.clear(); // 현재 살아있는 적 초기화
 
                     for(int ae=0; ae<aliveEnemy.size(); ae++){
-                        enemies.push_back(aliveEnemy[ae]);
+                        enemies.push_back(aliveEnemy[ae]); // 현재 살아있는 적들로 적 vector 갱신
                     }
 
-//                    bzero(tempMap, sizeof(tempMap));
-//
-//                    for(int e=0; e<enemies.size(); e++){
-//                        tempMap[enemies[e].x][enemies[e].y] = 1;
-//                    }
-//
-//                    cout << "After" << '\n';
-//
-//                    for(int r=1; r<=N; r++){
-//                        for(int c=0; c<M; c++){
-//                            cout << tempMap[r][c] << ' ';
-//                        }
-//                        cout << '\n';
-//                    }
-
-                    cout << '\n';
                 }
 
-                cout << "totalKill: " << kill << '\n';
-                cout << "======================================================" << '\n';
-
-                maxKill = maxKill < kill ? kill : maxKill;
+                maxKill = maxKill < kill ? kill : maxKill; // 전부 진행한 후 킬수 최대값 갱신
             }
         }
     }
