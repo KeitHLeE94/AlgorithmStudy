72f9c596d9f89da072c427e6dc600de40743f5a0
KeitHLeE94 <wogus4187@naver.com>
2019-06-19 10:17:20 +0900

    6/19 문제풀이

--- Baekjoon/17136.cpp
+++ Baekjoon/17136.cpp
@@ -6,11 +6,13 @@
 
 using namespace std;
 
-int minValue = 999;
+int minValue = 30;
+bool start = false;
 
 int map[10][10] = {0, };
+int limit[5] = {5, 5, 5, 5, 5};
 
-void update(int r, int c, int size){
+void change(int r, int c, int size){
     for(int i=r; i<r+size; i++){
         for(int j=c; j<c+size; j++){
             map[i][j] = 2;
@@ -29,7 +31,7 @@ void revoke(int r, int c, int size){
 bool fit(int r, int c, int size){
     for(int i=r; i<r+size; i++){
         for(int j=c; j<c+size; j++){
-            if(map[i][j] == 0 || map[i][j] == 2){
+            if(map[i][j] == 0 || map[i][j] == 2 || i >= 10 || j >= 10){
                 return false;
             }
         }
@@ -38,19 +40,7 @@ bool fit(int r, int c, int size){
     return true;
 }
 
-void DFS(int r, int c, int used[5]){
-    for(int i=0; i<10; i++){
-        for(int j=0; j<10; j++){
-            cout << map[i][j] << ' ';
-        }
-        cout << '\n';
-    }
-    for(int i=0; i<5; i++){
-        cout << used[i] << ' ';
-    }
-    cout << '\n';
-    cout << "========================================" << '\n';
-
+void DFS(int r, int c, int count){
     bool finish = true;
 
     for(int i=r; i<10; i++){
@@ -69,54 +59,46 @@ void DFS(int r, int c, int used[5]){
     }
 
     if(finish){
-        int result = 0;
-
-        for(int i=0; i<5; i++){
-            if(used[i] > 5){
-                result = -1;
-                break;
-            }
-            else{
-                result += used[i];
-            }
-        }
-
-        if(result >= 0){
-            minValue = minValue < result ? minValue : result;
-        }
+        minValue = minValue < count ? minValue : count;
 
         return;
     }
 
     for(int size=1; size<=5; size++){
-        if(fit(r, c, size)){
-            used[size-1]++;
-            update(r, c, size);
-            DFS(r, c, used);
-            used[size-1]--;
-            revoke(r, c, size);
+        if(limit[size-1] == 0){
+            continue;
         }
+
+        if(!fit(r, c, size)){
+            return;
+        }
+
+        limit[size-1]--;
+        change(r, c, size);
+        DFS(r, c, count+1);
+        revoke(r, c, size);
+        limit[size-1]++;
     }
 }
 
 int main(){
-    int map[10][10] = {0, };
-
     for(int i=0; i<10; i++){
         for(int j=0; j<10; j++){
             cin >> map[i][j];
+
+            if(map[i][j] == 1){
+                start = true;
+            }
         }
     }
 
-    int used[5] = {0, };
+    DFS(0, 0, 0);
 
-    DFS(0, 0, used);
-
-    if(minValue != 999){
-        cout << minValue << '\n';
+    if(start && minValue == 30){
+        cout << -1 << '\n';
     }
     else{
-        cout << -1 << '\n';
+        cout << minValue << '\n';
     }
 
     return 0;
